ec5836c7bd0c76b14abe089d41aa879f8adc10af
WojciechMalik <wojciech.malik@student.pk.edu.pl>
2021-10-12 10:46:25 +0200

    2

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main(){
-    std::cout<<"Hello World";
+    //powtorzenie z petli
+    for(int i=0; i<5; ++i){
+        cout<< "JiPP2" <<endl;
+    }
+
+    int tablica[2][3]={{1,2,3},{4,5,6}};
 
     return 0;
 }
