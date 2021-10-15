f0450c6e1b2bd163fc5cd95122edec6fa43fdbd2
WojciechMalik <wojciech.malik@student.pk.edu.pl>
2021-10-15 15:52:32 +0200

    Zadania z zajec

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -1,12 +1,52 @@
 #include <iostream>
 using namespace std;
+
+void tablica(int size){
+    int tablicka[5][5][5];
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            cout<<"{";
+            for(int k=0; k<size; k++){
+                tablicka[i][j][k]=rand()%9;
+                cout<<tablicka[i][j][k]<<" ";
+            }
+            cout<<"}";
+        }
+        cout<<"\n";
+    }
+}
+
+void add(int a, int b, int &c) {
+    c = a + b;
+}
+double add(double a,double b,double c,double d){
+    return a+b+c+d;
+}
+
 int main(){
-    //powtorzenie z petli
-    for(int i=0; i<5; ++i){
-        cout<< "JiPP2" <<endl;
+    int wybor, size=5, k;
+    cout<<"Ktore zadanie chcesz wyswietlic\n1-Powtorzenie z petli\n2-Tablica\n3-Przeciazenie \n";
+    cin>>wybor;
+    switch (wybor){
+        case 1:
+            //powtorzenie z petli
+            cout<<"Prosze napisac petle, ktora wyswietli 5 razy napis JiPP2.\n";
+            for(int i=0; i<5; ++i){
+                cout<< "JiPP2" <<endl;
+            }
+            break;
+        case 2:
+            cout<<"Prosze utworzyc dowolna tablice dwuwymiarowa oraz dla chetnych trojwymiarowa.\n";
+            tablica(size);
+            break;
+        case 3:
+            cout<<"Prosze utworzyc przeciazenie do powyzszej funkcji";
+            add(5, 5, k);
+            cout << k << endl;
+            cout<<"Przeciazenie funkcji"<<add(1.11, 2.22, 3.33, 4.44) <<"\n";
+            break;
     }
 
-    int tablica[2][3]={{1,2,3},{4,5,6}};
 
     return 0;
 }
